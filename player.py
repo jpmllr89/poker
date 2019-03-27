b4749a6f29972275dfbd6f4d8880975a1d884f18
jpmllr89 <jpmllr89@gmail.com>
2019-03-27 14:09:09 -0400

    Added functionality and init to player class

--- player.py
+++ player.py
@@ -1,5 +1,18 @@
 
 
 class Player:
-    __init__(self, name)
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+    def discard():
+        return hand.pop()
     
+    def draw_card(card):
+        hand.append(card)
+        return hand
+
+    def show_hand():
+        for i in hand:
+            print(i)
+        return hand
