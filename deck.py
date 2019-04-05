c7f1b92ebbe09a91e9aff4efd1dfc82bcd7bad48
jpmllr89 <jpmllr89@gmail.com>
2019-04-05 11:57:59 -0400

    added new functionality to deck class to resemble actions of dealing.  Made a globals file to partition variables out of classes themselves.

--- deck.py
+++ deck.py
@@ -1,19 +1,44 @@
-import cards
+from cards import Card
 from random import choice
 
-class Deck:
+_deck = []
 
-	_deck = []
-
+class Deck():
 
 	def __init__(self):
-		for i in range(0, 53):
+		for i in range(0, 52):
 			_deck.append(Card())
 
 
-	def count_deck():
+	def count_deck(self):
 		return len(_deck)
 
 
+	def __repr__(self):
+		return f"Deck of {self.count_deck()} cards"
+
+
+	def _deal(self, num):
+		try:
+			return _deck.pop()
+
+		except ValueError:
+			return "All cards have been dealt"
+
+
+	def deal_card(self):
+		return _deal(1)
+
+
+	def deal_hand(self, num):
+		hand = []
+		for i in range(0, num):
+			hand.append(deal_card(self))
+
+		return hand
+
 decker = Deck()
-print(decker._deck)
+print(decker.count_deck())
+decker.deal_hand(5)
+
+print(decker.hand)
